cb1271b1ce0040e2e32ec496684c87a036646129
Gambit-03 <g63987087@gmail.com>
2026-09-04 21:07:37 +0530

    Add: longest-turbulent-subarray - submission-0

---
+++ Data Structures & Algorithms/longest-turbulent-subarray/submission-0.cpp
@@ -0,0 +1,32 @@
+class Solution {
+    vector<vector<int>> memo;
+public:
+    int maxTurbulenceSize(vector<int>& arr) {
+        int n = arr.size();
+        memo.assign(n,vector<int>(2,-1));
+        int maxLen = 1;
+        for (int i =0;i<n;i++){
+            maxLen = max(maxLen, dfs(i,true,arr));
+            maxLen = max(maxLen, dfs(i,false,arr));
+
+
+        }
+
+        return maxLen;
+    }
+
+    int dfs(int i, bool sign, vector<int>& arr){
+        int signIndex = sign ? 1 : 0;
+        if(i==arr.size()-1) return 1;
+        if(memo[i][signIndex] != -1){
+            return memo[i][signIndex];
+        }
+
+        int result = 1;
+        if((sign&&arr[i] > arr[i+1])|| (!sign&&arr[i] < arr[i+1])){
+            result = 1 + dfs(i+1,!sign,arr);
+        }
+        memo[i][signIndex] = result;
+        return result;
+    }
+};
